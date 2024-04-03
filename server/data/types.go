6c558e4386a2b192bbfbc9f54f44701cb03e04da
sheeiavellie <josephlandmars@gmail.com>
2024-04-03 11:33:27 +0300

    fix: fix unmarshalling

--- server/data/types.go
+++ server/data/types.go
@@ -3,21 +3,21 @@ package data
 import "fmt"
 
 type ProcessRequest struct {
-	Timestamp string       `json:"timestamp"`
-	Data      []SensorData `json:"data"`
+	Timestamp string              `json:"timestamp"`
+	Data      []PotentiometerData `json:"data"`
 }
 
 type SensorData interface {
 	GetSensorData() string
 }
 
-type Potentiometer struct {
+type PotentiometerData struct {
 	ID         int    `json:"id"`
 	SensorType string `json:"sensor_type"`
 	Value      int    `json:"value"`
 }
 
-func (pd *Potentiometer) GetSensorType() string {
+func (pd *PotentiometerData) GetSensorData() string {
 	return fmt.Sprintf(
 		"| id: %d | type: %s | value: %d |",
 		pd.ID, pd.SensorType, pd.Value,
